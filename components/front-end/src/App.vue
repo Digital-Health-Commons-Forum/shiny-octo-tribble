54568efdea7dbd696b439973d11d70e10a6254f7
Alex O'Connor <alex@cloudient.net>
2024-12-05 16:46:33 +0000

    Clean up app.vue style

--- components/front-end/src/App.vue
+++ components/front-end/src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
-  import { RouterLink, RouterView } from 'vue-router'
+  import { RouterView } from 'vue-router'
   import NavBar from './components/nav/NavBar.vue';
 </script>
 
 <template>
   <v-app id="dhcf">
     <NavBar/>
-    <RouterView style="margin-top: 150px; margin-left: 64px; margin-right: 64px;"/>
+    <RouterView style="margin-top: 80px; margin-bottom: 20px;"/>
   </v-app>
 </template>
