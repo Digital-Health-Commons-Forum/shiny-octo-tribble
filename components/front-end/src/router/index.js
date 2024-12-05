a59f2cbc21afbfab8c306c0d9bbabffaffd0a4a3
Alex O'Connor <alex@cloudient.net>
2024-12-05 16:47:09 +0000

    add new route

--- components/front-end/src/router/index.js
+++ components/front-end/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import DocumentView from '@/views/DocumentView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,11 +10,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
-    // {
-    //   path: '/file/:filename',
-    //   name: 'file',
-    //   component: HomeView
-    // }
+    {
+      path: '/document/:doc_id',
+      name: 'document',
+      component: DocumentView
+    }
   ],
 })
 
