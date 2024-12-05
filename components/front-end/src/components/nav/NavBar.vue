cc9b3c47d4141fb9d6974ebe2e715cab488713c1
Alex O'Connor <alex@cloudient.net>
2024-12-05 16:46:46 +0000

    Add route link to header icon

--- components/front-end/src/components/nav/NavBar.vue
+++ components/front-end/src/components/nav/NavBar.vue
@@ -1,11 +1,16 @@
 <script setup>
   import IconDHCF from '../logos/DHCF.vue'
+  import { useRouter } from 'vue-router';
+
+  const router = useRouter();
 </script>
 
 <template>
   <v-app-bar>
     <v-app-bar-title>
-      <IconDHCF :height="'40px'"/>
+      <router-link to="/">
+        <IconDHCF :height="'40px'"/>
+      </router-link>
     </v-app-bar-title>
   </v-app-bar>
 </template>
