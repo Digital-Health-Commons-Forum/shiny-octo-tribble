41c4f09d9ba13edbd3a80705c043093ed86d0d91
Paul G Webster <paul.g.webster@googlemail.com>
2024-12-28 12:04:30 +0000

    Added python template code

--- components/worker-template-perl/template.pl
+++ components/worker-template-perl/template.pl
@@ -13,9 +13,27 @@ our $VERSION = '0.026';
 use POE;
 use LWP::UserAgent;
 use JSON::MaybeXS ':all';
+use Data::Dumper::Concise;
+
+# Details about this worker
+my $worker_info = {
+    'name'          => 'worker-template-perl',
+    'description'   => 'This is a template worker written in Perl.',
+    'author'        => 'PGW'
+};
+
+# A place to store worker state
+my $worker_state = {
+    'status'        => 'running',
+    'progress'      => 0,
+    'message'       => 'Worker is running...',
+    'data'          => {
+        'stage'    => 1,
+    }
+};
 
 # OpenAPI server URL
-my $api_url = 'http://127.0.0.1:8080/api';
+my $api_url = 'http://127.0.0.1:3000/';
 
 # Session to interact with OpenAPI server
 POE::Session->create(
@@ -44,14 +62,40 @@ sub start_interact_with_openapi {
 
 # Function to interact with OpenAPI server
 sub interact_with_openapi {
+    # Should really use a proper HTTP client library here, but this is just a template
     my $ua = LWP::UserAgent->new;
-    my $response = $ua->get($api_url);
 
-    if ($response->is_success) {
-        my $data = decode_json($response->decoded_content);
-        print "Received data from OpenAPI server: ", $data, "\n";
-    } else {
-        die "Failed to connect to OpenAPI server: ", $response->status_line;
+    # If we are state 1, we need to make a POST request
+    # and tell OpenAPI server that we are ready to start
+    # and who we are
+    if ($worker_state->{'data'}->{'stage'} == 1) {
+        my $payload = encode_json($worker_info);
+        my $api_target = $api_url . "worker";
+        
+        say "Stage(1): Sending worker info to OpenAPI server...";
+        my $response = $ua->post(
+            $api_target,
+            'Content-Type' => 'application/json',
+            'Content' => $payload
+        );
+
+        if ($response->is_success) {
+            my $data = decode_json($response->decoded_content);
+            say "Received data from OpenAPI server: ", Dumper $data;
+            $worker_info->{'data'}->{'stage'} = 2;
+        } else {
+            die "Failed to connect to OpenAPI server: ", $response->status_line;
+        }
+    }
+    else {
+        my $response = $ua->get($api_url);
+
+        if ($response->is_success) {
+            my $data = decode_json($response->decoded_content);
+            say "Received data from OpenAPI server: ", Dumper $data;
+        } else {
+            die "Failed to connect to OpenAPI server: ", $response->status_line;
+        }
     }
 }
 
@@ -62,6 +106,6 @@ sub start_additional_operation {
 
 # Additional operation function
 sub additional_operation {
-    print "Performing additional operation...\n";
+    say "Performing additional operation...\n";
     # Add your additional operation code here
 }
