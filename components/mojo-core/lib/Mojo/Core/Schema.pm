f14c4afe9d563bf730f788f1425ba32711651b0d
Paul G Webster <paul.g.webster@googlemail.com>
2025-07-09 19:26:19 +0100

    Bump version

--- components/mojo-core/lib/Mojo/Core/Schema.pm
+++ components/mojo-core/lib/Mojo/Core/Schema.pm
@@ -8,7 +8,7 @@ use v5.30;
 
 use Carp::Always;
 
-our $VERSION = '0.027';
+our $VERSION = '0.028';
 
 use base 'DBIx::Class::Schema';
 __PACKAGE__->load_namespaces;
--- components/mojo-core/lib/Mojo/Core/Schema/Result/Worker.pm
+++ components/mojo-core/lib/Mojo/Core/Schema/Result/Worker.pm
@@ -3,7 +3,7 @@ use DBIx::Class::Candy -autotable => v1;
 
 use warnings;
 use strict;
-our $VERSION = '0.027';
+our $VERSION = '0.028';
 
 primary_column id => {
     data_type => 'integer',
